191a15862da5e883a587893f61281f4f243a2f06
ltdungg <dungluong54321@gmail.com>
2025-08-22 11:41:48 +0700

    .

--- start.bat
+++ start.bat
@@ -1,2 +1,2 @@
-"C:\Program Files\Java\jdk-17\bin\java.exe" -Xmx10G -jar fabric-server-mc.1.20.1-loader.0.16.14-launcher.1.0.3.jar nogui
+"C:\Program Files\Java\jdk-17\bin\java.exe" -Xms9G -jar fabric-server-mc.1.20.1-loader.0.16.14-launcher.1.0.3.jar nogui
 pause
